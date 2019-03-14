f54eb73ab4faf5a55c18152c35711b39b503daaf
Bmahe <mahe.baptiste.19@gmail.com>
2019-03-15 00:09:09 +0100

    added database tracking and generation loop

--- imgGenerator.py
+++ imgGenerator.py
@@ -1,40 +1,51 @@
 from PIL import Image, ImageDraw
 from random import randrange
 from time import time
+import os
 
-# base variables
+# global variables
 
-size = 200
+size = 90
 resolution = 10
 num = int(size / resolution)
 grid = []
-
+databasePath = os.path.dirname(os.path.abspath(__file__)) + "\\database\\"
+imgId = str(int(open(databasePath + "tracks.txt", "r").read()))
+nbImgs = 2
 
 t = time()
-# generating a random grid
 
-grid = [[randrange(0, 2) for i in range(0, num)] for j in range(0, num)]
+for n in range(0, nbImgs):
 
-# set up the drawing
+    imgId = str(int(imgId) + 1)
 
-img = Image.new('RGB', (size, size), 'white')
-draw = ImageDraw.Draw(img)
+    # generating a random grid
 
-# draw a black rectangle for every 1 in the grid
+    grid = [[randrange(0, 2) for i in range(0, num)] for j in range(0, num)]
 
-for i in range(0, num):
-    for j in range(0, num):
-        if grid[i][j] == 1:
-            x0, y0 = i * resolution, j * resolution
-            x1, y1 = (i + 1) * resolution, (j + 1) * resolution
-            draw.rectangle([x0, y0, x1, y1], fill='black')
+    # set up the drawing
 
-# name and save the result
+    img = Image.new('RGB', (size, size), 'white')
+    draw = ImageDraw.Draw(img)
 
-filename = "pil_img.png"
-img.save(filename)
+    # draw a black rectangle for every 1 in the grid
+
+    for i in range(0, num):
+        for j in range(0, num):
+            if grid[i][j] == 1:
+                x0, y0 = i * resolution, j * resolution
+                x1, y1 = (i + 1) * resolution, (j + 1) * resolution
+                draw.rectangle([x0, y0, x1, y1], fill='black')
+
+    # name and save the result
+
+    file = databasePath + imgId + "img.png"
+    img.save(file)
+
+# update the tracking
+
+open(databasePath + "tracks.txt", "w").write(imgId)
 
 # img.show()
 
-print("time : " + str(round((time() - t)*1000)) + "ms")
-
+print("total time : " + str(round((time() - t) * 1000)) + "ms")
