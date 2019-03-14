df451f21dd74883e7242e3a949109ce02b3a18cb
Bmahe <mahe.baptiste.19@gmail.com>
2019-03-14 17:43:09 +0100

    updated imgGenerator.py

--- imgGenerator.py
+++ imgGenerator.py
@@ -1,21 +1,18 @@
 from PIL import Image, ImageDraw
 from random import randrange
+from time import time
 
-#base variables
+# base variables
 
 size = 100
 resolution = 10
-num = int(size/resolution)
+num = int(size / resolution)
 grid = []
 
 # generating a random grid
+t = time()
 
-for i in range(0, num):
-    col = []
-    for j in range(0, num):
-        col.append(randrange(0, 2))
-    grid.append(col)
-
+grid = [[randrange(0, 2) for i in range(0, num)] for j in range(0, num)]
 # set up the drawing
 
 img = Image.new('RGB', (size, size), 'white')
@@ -27,10 +24,12 @@ for i in range(0, num):
     for j in range(0, num):
         if grid[i][j] == 1:
             x0, y0 = i * resolution, j * resolution
-            x1, y1 = (i + 1) * resolution, (j+1) * resolution
+            x1, y1 = (i + 1) * resolution, (j + 1) * resolution
             draw.rectangle([x0, y0, x1, y1], fill='black')
 
 # name and save the result
 
 filename = "pil_img.png"
 img.save(filename)
+
+print(time() - t)
